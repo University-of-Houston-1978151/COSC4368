3ecbb9c50d258db3cf423d3080dbd4119f6cf70e
ash14545 <russellfar@hotmail.com>
2022-02-15 23:17:30 -0600

    Rename app.py to RHC.py

--- RHC.py
+++ RHC.py
@@ -1,13 +1,17 @@
 import math
+from termios import IXOFF
 
-x = 50
-y = 12
+x = int(input("Enter a value for x:\n"))
+y = int(input("Enter a value for y:\n"))
 
 if x > 512 or x < -512:
     print("\"x\" must be within the [-512, 512] range.")
+    exit()
 
 if y > 512 or y < -512:
     print("\"y\" must be within the [-512, 512] range.")
+    exit()
 
+print()
 print(x)
 print(y)
