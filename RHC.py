d8786450ac58eb2490231bfd922119113ab4a48c
ash14545 <russellfar@hotmail.com>
2022-02-16 02:44:07 -0600

    Update RHC.py
    Print starting points

--- RHC.py
+++ RHC.py
@@ -1,4 +1,6 @@
 import math
+import random
+import os
 
 def checkRange(num):
     if num > 512 or num < -512:
@@ -11,5 +13,7 @@ y = int(input("Enter a value for y:\n"))
 checkRange(y)
 
 print()
-print(x)
-print(y)
+print(f"Starting points at: ({x}, {y})")
+term_size = os.get_terminal_size()
+print(u'\u2500' * term_size.columns)
+print()
