a2e3d9e46fd590bd3cf4380d45cc4782ab5d2a89
ash14545 <russellfar@hotmail.com>
2022-02-15 22:59:38 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import math
+
+x = 50
+y = 12
+
+if x > 512 or x < -512:
+    print("\"x\" must be within the [-512, 512] range.")
+
+if y > 512 or y < -512:
+    print("\"y\" must be within the [-512, 512] range.")
+
+print(x)
+print(y)
