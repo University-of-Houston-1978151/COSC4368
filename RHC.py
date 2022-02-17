0c0a22406551e1ff0ce6ede33114dc01e18acf68
ash14545 <russellfar@hotmail.com>
2022-02-17 00:10:16 -0600

    Update RHC.py

--- RHC.py
+++ RHC.py
@@ -11,9 +11,11 @@ def checkRange(num):
         exit()
 
 
-x = int(input("Enter a value for x:\n"))
+localMin = float()
+
+x = float(input("Enter a value for x:\n"))
 checkRange(x)
-y = int(input("Enter a value for y:\n"))
+y = float(input("Enter a value for y:\n"))
 checkRange(y)
 
 p = int(input("Enter a value for p:\n"))
@@ -23,8 +25,10 @@ z = [-inputz, inputz]
 seed = str(input("Would you like to use a seed? (y/n)\n"))
 if seed == "y" or seed == "yes":
     seed = float(input("What would you like to use for the seed?\n"))
+    random.seed(seed)
 else:
     seed = None
+    random.seed()
 
 # Print (x, y), p, and z
 print()
@@ -47,20 +51,39 @@ def f(x, y):
     )
     return ans
 
-# Generate and run with new "z"
+
+def zFuncX(x):
+    newX = x + random.uniform(z[0], z[1])
+
+    if x > 512 or x < -512:
+        zFuncX(x)
+    else:
+        return newX
 
 
-def zFunc(x, y, z):
-    pass
+def zFuncY(y):
+    newY = y + random.uniform(z[0], z[1])
+
+    if y > 512 or y < -512:
+        zFuncY(y)
+    else:
+        return newY
 
 
 # Initial (x, y)
+localMin = f(x, y)
+
+
+def RHC(x, y, p, z):
+    for i in range(1, p+1):
+        newX = zFuncX(x)
+        newY = zFuncY(y)
+
+        print(f"{newX}, {newY}")
+
+
 print("Initial (x, y):")
-print(f"f({x}, {y}) = {f(x,y)}")
+print(f"f({x}, {y}) = {localMin}")
 print()
 
-# "p" runs
-for i in range(1, p+1):
-    print(f"{i} | f({x}, {y}) = {f(x,y)}")
-
-# Check if "p" runs has any minimums. If true, run function "p" times, else done
+RHC(x, y, p, z)
